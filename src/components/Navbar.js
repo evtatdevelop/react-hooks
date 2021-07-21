36051698f675a6889b4b731fcae4deacd77299fb
Evgenii <evtatdevelop@gmail.com>
2021-07-21 16:55:27 +0700

    added bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,19 @@ export const Navbar = () => (
       <div className = "navbar-brand">
         Github Search
       </div>
-      <ul className = "navbar-nav">
-        <li className = "nav-item">
-          <NavLink to="/" exact className = "nav-link">Home</NavLink>
-        </li>
-        <li className = "nav-item">
-          <NavLink to="/about" className = "nav-link">About</NavLink>
-        </li>
-      </ul>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className = "navbar-nav">
+          <li className = "nav-item">
+            <NavLink to="/" exact className = "nav-link">Home</NavLink>
+          </li>
+          <li className = "nav-item">
+            <NavLink to="/about" className = "nav-link">About</NavLink>
+          </li>
+        </ul>
+      </div>  
     </div>  
 
   </nav>
